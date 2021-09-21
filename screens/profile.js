86c0f4514f32c209cdad9ca45cd1d1038e4ef815
Jake Goeldel <goeldelj@gmail.com>
2021-09-21 14:28:08 -0400

    setup test patient screen

--- screens/profile.js
+++ screens/profile.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const ProfileScreen = ({ navigation, route }) => {
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text>Patient Profile</Text>
+			<Text style={styles.title}>Patient Profile</Text>
+			<Text style={styles.bold}>Name: <Text style={styles.information}>test</Text></Text>
+			<Text style={styles.bold}>ID: <Text style={styles.information}>test</Text></Text>
+			<Text style={styles.bold}>Zipcode: <Text style={styles.information}>test</Text></Text>
+			<Text style={styles.bold}>Birthdate: <Text style={styles.information}>test</Text></Text>
 		</View>
 	);
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+	title: {
+		fontWeight: "bold",
+		fontSize: 32
+	},
+	bold: {
+		fontWeight: "bold",
+		fontSize: 16
+	},
+	information: {
+		fontWeight: "normal",
+		fontSize: 16
+	}
+});
