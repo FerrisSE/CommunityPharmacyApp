77e8481238f931b94fd92bf47823953c0b606b0e
Jacob Goeldel <goeldej@ferris.edu>
2021-09-27 20:10:18 -0400

    Patient Profile screen pulls data from backend

--- screens/profile.js
+++ screens/profile.js
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const ProfileScreen = ({ navigation, route }) => {
-	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text style={styles.title}>Patient Profile</Text>
-			<Text style={styles.bold}>Name: <Text style={styles.information}>test</Text></Text>
-			<Text style={styles.bold}>ID: <Text style={styles.information}>test</Text></Text>
-			<Text style={styles.bold}>Zipcode: <Text style={styles.information}>test</Text></Text>
-			<Text style={styles.bold}>Birthdate: <Text style={styles.information}>test</Text></Text>
-		</View>
-	);
+	let [patient, setPatient] = React.useState('')
+	let [loading, setLoading] = React.useState(true)
+	let [error, setError] = React.useState(false)
+
+	useEffect(() => {
+		fetch('http://localhost:8080/LocalPatient/Martin/Kucharek', {
+			"method": "GET",
+			"headers": {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+		}).then(response => response.json())
+			.then(response => setPatient(response))
+			.catch(err => setError(err))
+			.finally(() => setLoading(false))
+	}, []);
+
+
+	if (error)
+		return <ProfileFailed error_code={error} />
+	if (loading)
+		return <ProfileLoading />
+
+	return <Profile patient={patient[0]} />
 };
 
 export default ProfileScreen;
+
+function ProfileLoading(props) {
+	return <View style={styles.center}>
+		<Text style={styles.center}>Loading</Text>
+	</View>
+}
+
+function ProfileFailed(props) {
+	return <View style={styles.center}>
+		<Text style={styles.errorText}>Something Went Wrong!</Text>
+	</View>
+}
+
+function Profile(props) {
+	console.log(props)
+	return <View style={{ flex: 1, alignItems: 'center' }}>
+		<Text style={styles.title}>Patient Profile</Text>
+		<ProfileField text="Name:" value={props.patient.givenName + ' ' + props.patient.familyName} />
+		<ProfileField text="Street:" value={props.patient.addressStreet} />
+		<ProfileField text="City: " value={props.patient.addressCity} />
+		<ProfileField text="State:" value={props.patient.addressState} />
+		<ProfileField text="Zipcode:" value={props.patient.addressZipcode} />
+		<ProfileField text="Birthday:" value={props.patient.birthdate} />
+		<ProfileField text="Gender: " value={props.patient.gender} />
+	</View>
+}
+
+function ProfileField(props) {
+	return <Text style={styles.bold}>{props.text} <Text style={styles.information}>{props.value}</Text></Text>
+}
 
 const styles = StyleSheet.create({
 	title: {
@@ -27,5 +72,15 @@ const styles = StyleSheet.create({
 	information: {
 		fontWeight: "normal",
 		fontSize: 16
+	},
+	errorText: {
+		fontWeight: "bold",
+		fontSize: 24,
+		color: "crimson"
+	},
+	center: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
 	}
 });
