aff48d91a9107b6c1edd1d9c83910d43966748ba
Jacob Goeldel <goeldej@ferris.edu>
2021-10-05 13:04:31 -0400

    changed to drawer navigation

--- App.js
+++ App.js
@@ -1,37 +1,19 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import HomeScreen from './screens/home';
 import ProfileScreen from './screens/profile';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Home' }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ title: 'My Profile' }}
-        />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="My Profile" component={ProfileScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/home.js
+++ screens/home.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 			<Text>Home Screen</Text>
-			<Button
-				title="Patient Profile Screen"
-				onPress={() => navigation.push('Profile')}
-			/>
 		</View>
 	);
 };
