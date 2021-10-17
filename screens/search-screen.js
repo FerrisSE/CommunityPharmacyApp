6eba7222563e30d95c8ece01d1cd7d6624a5f53e
Jacob Goeldel <goeldej@ferris.edu>
2021-10-17 16:51:40 -0400

    added patient search

--- screens/search-screen.js
+++ screens/search-screen.js
@@ -1,34 +1,73 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
+import TextLink from '../components/text-link';
+import mainStyles from '../main-styles';
+
+const SearchForPatient = (firstName, familyName, birthdate, setPatients) => {
+	fetch(`http://localhost:8080/api/patient/name/${firstName}/${familyName}/${birthdate}`, {
+		"method": "GET",
+		"headers": {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+	}).then(response => response.json())
+		.then(response => setPatients(response))
+		.catch(err => {
+			console.log(err)
+			return []
+		})
+}
 
 const SearchScreen = ({ navigation }) => {
+	let [patients, setPatients] = React.useState([])
+	let [givenName, setGivenName] = React.useState('')
+	let [familyName, setFamilyName] = React.useState('')
+	let [birthdate, setBirthdate] = React.useState('')
+
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text>Search</Text>
-			<Button
-				title="Patient Profile Screen"
-				onPress={() => navigation.navigate({
-					name: 'Patient Profile',
-					params: {
-						patient: {
-							"givenName": "Joe",
-							"familyName": "Bob",
-							"phoneNumber": "123456789",
-							"gender": "male",
-							"birthdate": "2020-09-14",
-							"addressStreet": "nothing",
-							"addressZipcode": "456465",
-							"addressCity": "nothing",
-							"addressState": "nothing",
-							"status": true,
-							"consentTimeStamp": "2020-09-14",
-							"conditions": null,
-							"medications": null,
-							"allergies": null
-						}
-					}
-				})}
-			/>
+		<View style={mainStyles.container}>
+			<Text style={mainStyles.title}>Patient Search</Text>
+
+			<View style={mainStyles.CardView}>
+				<Text style={mainStyles.subheader}>Given Name</Text>
+				<TextInput
+					style={mainStyles.textInput}
+					placeholder="given name"
+					onChangeText={text => setGivenName(text)}
+					defaultValue={givenName}
+				/>
+
+				<Text style={mainStyles.subheader}>Family Name</Text>
+				<TextInput
+					style={mainStyles.textInput}
+					placeholder="familty name"
+					onChangeText={text => setFamilyName(text)}
+					defaultValue={familyName}
+				/>
+
+				<Text style={mainStyles.subheader}>Birthdate</Text>
+				<TextInput
+					style={mainStyles.textInput}
+					placeholder="birthday (year-month-day)"
+					onChangeText={text => setBirthdate(text)}
+					defaultValue={birthdate}
+				/>
+				<Button title="Search" onPress={() => SearchForPatient(givenName, familyName, birthdate, setPatients)} />
+			</View>
+
+			{patients.map((patient) => {
+				return (
+					<TextLink label={`${patient.givenName} ${patient.familyName}, ${patient.birthdate}`} onPress={() => {
+						navigation.navigate({
+							name: 'Patient Profile',
+							params: {
+								patient: patient
+							}
+						})
+					}} />
+				)
+			})}
+
 		</View>
 	);
 };
