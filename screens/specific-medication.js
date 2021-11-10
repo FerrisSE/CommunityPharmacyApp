afe86bb2b8365173b000f1bbfd836fe2737244d5
Jacob Goeldel <goeldej@ferris.edu>
2021-11-10 17:32:15 -0500

    cleaned up medication screen and added videos

--- screens/specific-medication.js
+++ screens/specific-medication.js
@@ -1,73 +1,74 @@
 import React, { useEffect } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import mainStyles from './main-styles';
 import { Button, FlatList, SafeAreaView, Text, View } from 'react-native';
+import mainStyles from '../main-styles';
+import YoutubePlayer from "react-native-youtube-iframe";
 
 const SpecificMedicationScreen = ({ navigation, route }) => {
-	return (
-			<ScrollView>
-		<SafeAreaView style={mainStyles.container}>
-    <View style={mainStyles.center}>
-			<Text style={mainStyles.title}>Medication</Text>
-            </View>
-            <View style={mainStyles.center}>
-                <Icon name="pill" size={40} /> 
-                <View>
-                    <Text> {"Medication Brand Name"} </Text>
-                    <Text>{"Generic Name"}</Text>
-					
-			        	</View>
+  return (
+    <ScrollView>
+      <SafeAreaView style={mainStyles.container}>
+        <View style={mainStyles.center}>
+          <Text style={mainStyles.title}>{route.params.med.medicationName}</Text>
+        </View>
+        <View style={[mainStyles.center, { padding: 16 }]}>
+          <Icon name="pill" size={80} />
+          <View>
+            <Text>{route.params.med.drugPurpose}</Text>
+            <Text>{route.params.med.instructions}</Text>
+
+          </View>
 
 
-            </View>
+        </View>
 
-            <View >
-            <View style={{margin:10}}>
+        <View >
+          <View style={{ margin: 10 }}>
             <Button
               title="Refill"
               onPress={() => navigation.navigate('Profile')}
             />
-            </View>
-            <View style={{margin:10}}>
+          </View>
+          <View style={{ margin: 10 }}>
             <Button
               title="Infomation"
               onPress={() => navigation.navigate('Profile')}
             />
-             </View>
-             <View style={{margin:10}}>
+          </View>
+          <View style={{ margin: 10 }}>
             <Button
               title="Side Effects"
               onPress={() => navigation.navigate('Profile')}
             />
-            </View>
-            <View style={{margin:10}}>
+          </View>
+          <View style={{ margin: 10 }}>
             <Button
               title="Reminders"
               onPress={() => navigation.navigate('Profile')}
             />
-					</View>
-			      </View>
+          </View>
+        </View>
 
-            <View >
-                    
-              <Text> {"Instructions"} </Text>
-              <View style={mainStyles.CardView}>
-              <Text> {"Instructions for meds"} </Text>
+        <View >
 
-              <View style={mainStyles.center}>
-              <Icon name="laptop" size={150} />
-                </View>
-			      </View>
-            </View>
+          <Text> {"Instructions"} </Text>
+          <View style={mainStyles.CardView}>
+            <Text> {"Instructions for meds"} </Text>
+            <YoutubePlayer
+              height={300}
+              videoId={"iik25wqIuFo"}
+            />
+          </View>
+        </View>
 
-            
 
-			
-			{/* <TextLink label="Current Medication" onPress={() => { navigator.navigate('Current Medication') }} /> */}
-		</SafeAreaView>
-	</ScrollView>
-	);
+
+
+        {/* <TextLink label="Current Medication" onPress={() => { navigator.navigate('Current Medication') }} /> */}
+      </SafeAreaView>
+    </ScrollView>
+  );
 };
 
 export default SpecificMedicationScreen;
