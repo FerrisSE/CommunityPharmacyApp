506f45519c039de381d812668d80f97c72c3ee59
Jacob Goeldel <goeldej@ferris.edu>
2021-11-30 16:42:45 -0500

    added search bar to scheduling

--- screens/patient/scheduling/scheduling.js
+++ screens/patient/scheduling/scheduling.js
@@ -1,46 +1,85 @@
 import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 import SchedulingButton from '../../../components/scheduling-card-button';
+import mainStyles from '../../../main-styles';
 
 const vaccines = [
-	{ name: "Covid-19 Vaccine" },
-	{ name: "Influenza Vaccine" },
-	{ name: "Flu Vaccine" }
+	{ name: "Covid-19 Vaccine", icon: "bandage" },
+	{ name: "Influenza Vaccine", icon: "bandage" },
+	{ name: "Flu Vaccine", icon: "bandage" }
 ]
 
 const bloodTests = [
-	{ name: "AC-1 Test" },
-	{ name: "Whole Blood Glucose" },
-	{ name: "Hemoglobin Test" }
+	{ name: "AC-1 Test", icon: "water-outline" },
+	{ name: "Whole Blood Glucose", icon: "water-outline" },
+	{ name: "Hemoglobin Test", icon: "water-outline" }
 ]
 
 const SchedulingScreen = ({ navigation }) => {
+	let [searchText, setSearchText] = React.useState('')
+
+	let searchEvents = []
+	if (searchText.length != 0) {
+		searchEvents.push(
+			...vaccines.filter(v => v.name.toLowerCase().includes(searchText.toLowerCase())),
+			...bloodTests.filter(v => v.name.toLowerCase().includes(searchText.toLowerCase()))
+		);
+	}
+
 	return (
 		<ScrollView style={SchldStyles.container}>
-			<Text style={SchldStyles.title}>Scheduling Services</Text>
+			<Text style={SchldStyles.title}>Services</Text>
 
-			<Text style={SchldStyles.subtitle}>Vaccines</Text>
-			<FlatGrid
-				itemDimension={240}
-				data={vaccines}
-				style={SchldStyles.gridView}
-				spacing={10}
-				renderItem={({ item }) => (
-					<SchedulingButton icon="bandage" label={item.name} />
-				)}
+			<TextInput
+				style={mainStyles.textInput}
+				placeholder="search"
+				onChangeText={text => setSearchText(text)}
+				defaultValue={searchText}
 			/>
 
-			<Text style={SchldStyles.subtitle}>Blood Tests</Text>
-			<FlatGrid
-				itemDimension={240}
-				data={bloodTests}
-				style={SchldStyles.gridView}
-				spacing={10}
-				renderItem={({ item }) => (
-					<SchedulingButton icon="water-outline" label={item.name} />
-				)}
-			/>
+			{searchText.length == 0 &&
+				<View>
+					<Text style={SchldStyles.subtitle}>Vaccines</Text>
+					<FlatGrid
+						itemDimension={240}
+						data={vaccines}
+						style={SchldStyles.gridView}
+						spacing={10}
+						renderItem={({ item }) => (
+							<SchedulingButton icon={item.icon} label={item.name} />
+						)}
+					/>
+
+					<Text style={SchldStyles.subtitle}>Blood Tests</Text>
+					<FlatGrid
+						itemDimension={240}
+						data={bloodTests}
+						style={SchldStyles.gridView}
+						spacing={10}
+						renderItem={({ item }) => (
+							<SchedulingButton icon={item.icon} label={item.name} />
+						)}
+					/>
+				</View>
+			}
+
+			{searchText.length != 0 &&
+				<View>
+					<Text style={SchldStyles.subtitle}>Search Results For {searchText}...</Text>
+					<FlatGrid
+						itemDimension={240}
+						data={searchEvents}
+						style={SchldStyles.gridView}
+						spacing={10}
+						renderItem={({ item }) => (
+							<SchedulingButton icon={item.icon} label={item.name} />
+						)}
+					/>
+				</View>
+			}
+
+
 		</ScrollView>
 	);
 };
