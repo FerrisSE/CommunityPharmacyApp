86f411ae73808a0be115ae3d2fc4ec5324504c30
Jacob Goeldel <goeldej@ferris.edu>
2022-03-29 13:26:40 -0400

    finished upcoming events dashboard component

--- screens/patient/home.js
+++ screens/patient/home.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Pressable, SafeAreaView, Text, View } from 'react-native';
 import { AdherenceButtonSmall } from '../../components/adherence-button';
-import { TextSubHeader2 } from '../../components/text';
+import { TextBody, TextHeader2, TextSubHeader1, TextSubHeader2 } from '../../components/text';
 import { default as Icon } from "react-native-vector-icons/MaterialCommunityIcons";
 import { Card } from '../../components/cards';
 import { SECONDARY_COLOR } from '../../colors';
+import { useIsFocused } from '@react-navigation/native';
+import axios from 'axios';
+import moment from 'moment';
+import { SERVER_URL } from '../../constants';
+import { useSelector } from 'react-redux';
 
 export const HomeScreen = ({ navigation }) => {
 	const toScheduling = () => navigation.navigate("Scheduling");
+	const [nextEvent, setNextEvent] = useState();
+
+	const userToken = useSelector((state) => state.userToken.value);
+
+	// pull next event
+	const isFocused = useIsFocused();
+	useEffect(() => {
+		// get the clients list of scheduled events
+		axios({
+			method: 'get',
+			url: `${SERVER_URL}/api/schedule/patient/0`, // hardcoded to first user
+			headers: {
+				Authorization: userToken,
+			}
+		}).then(response => {
+			console.log(response.data.length);
+			if (response.data.length > 0) {
+				setNextEvent({
+					name: response.data[0].category,
+					date: moment(response.data[0].day),
+					time: moment(response.data[0].start, "HH:mm:ss")
+				});
+			}
+		});
+	}, [isFocused]);
 
 	return (
 		<View style={{ padding: 8 }}>
@@ -29,9 +59,22 @@ export const HomeScreen = ({ navigation }) => {
 							<View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
 								<Icon name="arrow-expand" size={20} color={SECONDARY_COLOR} />
 							</View>
-							<View style={{ flex: 1, justifyContent: "flex-end" }}>
-								<TextSubHeader2 text="No upcoming events" />
-							</View>
+							{nextEvent ?
+								<View style={{ flex: 1 }}>
+									<TextSubHeader2 text={nextEvent.date.format("dddd")} />
+
+									<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+										<TextHeader2 text={nextEvent.date.format("M/D")} />
+									</View>
+
+									<TextSubHeader2 text={nextEvent.name} />
+									<TextBody text={nextEvent.time.format("h:mm A")} />
+								</View>
+								:
+								<View style={{ flex: 1, justifyContent: "flex-end" }}>
+									<TextSubHeader2 text="No upcoming events" />
+								</View>
+							}
 						</Card>
 					</Pressable>
 				</View>
