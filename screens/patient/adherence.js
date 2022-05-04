1554ad67ed7a9f9174018e4f9c5ffe0398d23384
Jacob Goeldel <goeldej@ferris.edu>
2022-05-04 11:46:01 -0400

    connected to adherence backend

--- screens/patient/adherence.js
+++ screens/patient/adherence.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
-import { TextNote, TextSubHeader2 } from '../../components/text';
+import { TextSubHeader2 } from '../../components/text';
 import { Card } from "../../components/cards";
-import { AdherenceGraph, MedAdherenceCard } from "../../components/adherence-components";
-
-const meds = [
-	{ name: "Amoxicillian", taken: 24.0, skipped: 6.0 },
-	{ name: "Test Med", taken: 10.0, skipped: 5.0 }
-]
-
-const GetMedAdherence = (med) => med.taken / (med.taken + med.skipped);
+import { AdherenceGraph, GetAdherence, GetAdherencePrecent, MedAdherenceCard } from "../../components/adherence-components";
+import { useSelector } from "react-redux";
 
 export const AdherenceScreen = () => {
-	let overallAdherence = meds.reduce((t, m) => GetMedAdherence(t) + GetMedAdherence(m)) / meds.length;
-	let mostMissed = meds.reduce((c, m) => GetMedAdherence(c) < GetMedAdherence(m) ? c : m);
-	let leastMissed = meds.reduce((c, m) => GetMedAdherence(c) > GetMedAdherence(m) ? c : m);
+	let [meds, setMeds] = useState([]);
+
+	// default values for if meds can't be pulled in or there aren't any
+	let overallAdherence = 1;
+	let mostMissed = "none";
+	let leastMissed = "none";
+
+	if (meds.length == 1) {
+		overallAdherence = GetAdherencePrecent(meds[0]);
+	} else if (meds.length > 1) {
+		overallAdherence = meds.reduce((t, m) => GetAdherencePrecent(t) + GetAdherencePrecent(m)) / meds.length;
+		mostMissed = meds.reduce((c, m) => GetAdherencePrecent(c) < GetAdherencePrecent(m) ? c : m).name;
+		leastMissed = meds.reduce((c, m) => GetAdherencePrecent(c) > GetAdherencePrecent(m) ? c : m).name;
+	}
+
+	const userToken = useSelector((state) => state.userToken.value);
+
+	useEffect(async () => {
+		setMeds(await GetAdherence(userToken));
+	}, []);
 
 	return (
 		<ScrollView>
@@ -25,19 +36,19 @@ export const AdherenceScreen = () => {
 				</View>
 
 				<TextSubHeader2 text="By Medication" style={{ marginBottom: 8 }} />
-				{meds.map(m => <MedAdherenceCard med={m} key={m.name} />)}
+				{meds.map((m, i) => <MedAdherenceCard med={m} key={i} />)}
 
 				<View style={{ flex: 1, flexDirection: "row", marginTop: 16 }}>
 					<View style={{ width: "50%", padding: 8, aspectRatio: 1 }}>
 						<TextSubHeader2 text="Most Missed" style={{ marginLeft: 16 }} />
 						<Card depth={1} style={{ flex: 1, margin: 4, padding: 16, justifyContent: 'flex-end' }}>
-							<TextSubHeader2 text={mostMissed.name} />
+							<TextSubHeader2 text={mostMissed} />
 						</Card>
 					</View>
 					<View style={{ width: "50%", padding: 8, aspectRatio: 1 }}>
 						<TextSubHeader2 text="Least Missed" style={{ marginLeft: 16 }} />
 						<Card depth={1} style={{ flex: 1, margin: 4, padding: 16, justifyContent: 'flex-end' }}>
-							<TextSubHeader2 text={leastMissed.name} />
+							<TextSubHeader2 text={leastMissed} />
 						</Card>
 					</View>
 				</View>
