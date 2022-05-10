4e321502c16439ea6376ddbd927ebe58b72295e4
Jacob Goeldel <goeldej@ferris.edu>
2022-05-10 14:17:33 -0400

    added adherence posting to homescreen

--- screens/patient/home.js
+++ screens/patient/home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Pressable, SafeAreaView, Text, View } from 'react-native';
-import { AdherenceButtonSmall } from '../../components/adherence-components';
-import { TextBody, TextHeader2, TextSubHeader1, TextSubHeader2 } from '../../components/text';
+import { Image, Pressable, ScrollView, View } from 'react-native';
+import { AdherenceButtonSmall, GetAdherence } from '../../components/adherence-components';
+import { TextBody, TextHeader2, TextNote, TextSubHeader1, TextSubHeader2 } from '../../components/text';
+import pill from "../../images/pill.png"
 import { default as Icon } from "react-native-vector-icons/MaterialCommunityIcons";
 import { Card } from '../../components/cards';
-import { SECONDARY_COLOR, SECONDARY_COLOR_TRANSPARENT } from '../../colors';
+import { PRIMARY_COLOR, SECONDARY_COLOR, SECONDARY_COLOR_TRANSPARENT } from '../../colors';
 import { useIsFocused } from '@react-navigation/native';
+import { PrimaryButton } from '../../components/buttons';
 import axios from 'axios';
 import moment from 'moment';
 import { SERVER_URL } from '../../constants';
@@ -13,33 +15,104 @@ import { useSelector } from 'react-redux';
 
 export const HomeScreen = ({ navigation }) => {
 	const toScheduling = () => navigation.navigate("Scheduling");
+
+	let [adherenceMeds, setAdherenceMeds] = useState([]);
+	let [adherenceModel, setAdherenceModel] = useState([]);
 	const [nextEvent, setNextEvent] = useState();
 
 	const userToken = useSelector((state) => state.userToken.value);
 
+	const getMedsThisDay = (medId) => adherenceModel.find(m => m.medId == medId).medsPerDay[moment().day()];
+
+	const getMedAdherence = async () => {
+		try {
+			return await GetAdherence(userToken);
+		} catch (err) {
+			console.error(err);
+			return [];
+		}
+	}
+
+	const getAdherenceModel = async () => {
+		try {
+			let data = (await axios({
+				method: 'get',
+				url: `${SERVER_URL}/patient/adherence/models`,
+				headers: {
+					Authorization: userToken,
+				}
+			})).data;
+
+			let model = data.map(m => ({
+				medId: m.patientMedicationId,
+				medsPerDay: [
+					m.sundayPills,
+					m.mondayPills,
+					m.tuesdayPills,
+					m.wednesdayPills,
+					m.thursdayPills,
+					m.fridayPills,
+					m.saturdayPills,
+				]
+			}));
+
+			return model;
+		} catch (err) {
+			console.log(err);
+			return [];
+		}
+	}
+
+	const getNextEvent = async () => {
+		try {
+			// get the clients list of scheduled events
+			let data = (await axios({
+				method: 'get',
+				url: `${SERVER_URL}/api/schedule/`,
+				headers: {
+					Authorization: userToken,
+				}
+			})).data;
+
+			// grab the next event only
+			if (data.length > 0) {
+				return {
+					name: data[0].category,
+					date: moment(data[0].day),
+					time: moment(data[0].start, "HH:mm:ss")
+				};
+			}
+
+			return false;
+		} catch (err) {
+			console.error(err);
+			return false;
+		}
+	}
+
 	// pull next event
 	const isFocused = useIsFocused();
-	useEffect(() => {
-		// get the clients list of scheduled events
-		axios({
-			method: 'get',
-			url: `${SERVER_URL}/api/schedule/`,
-			headers: {
-				Authorization: userToken,
-			}
-		}).then(response => {
-			if (response.data.length > 0) {
-				setNextEvent({
-					name: response.data[0].category,
-					date: moment(response.data[0].day),
-					time: moment(response.data[0].start, "HH:mm:ss")
-				});
-			}
-		});
+	useEffect(async () => {
+		setNextEvent(await getNextEvent());
+		setAdherenceModel(await getAdherenceModel());
+		setAdherenceMeds(await getMedAdherence());
 	}, [isFocused]);
 
 	return (
-		<View style={{ padding: 8 }}>
+		<ScrollView style={{ padding: 8 }}>
+
+			{adherenceMeds.length != 0 &&
+				<View style={{ margin: 4, marginBottom: 24 }}>
+					<TextSubHeader2 text="Today's Medications" style={{ marginLeft: 16, marginBottom: 4 }} />
+					{adherenceMeds.map((m, i) => <AdherenceHomeCard
+						med={m}
+						medsThisDay={getMedsThisDay(m.patientMedicationId)}
+						refreshMeds={async () => setAdherenceMeds(await getMedAdherence())}
+						key={i}
+					/>)}
+				</View>
+			}
+
 
 			<View style={{
 				width: '100%',
@@ -81,6 +154,71 @@ export const HomeScreen = ({ navigation }) => {
 				</View>
 
 			</View>
-		</View>
+		</ScrollView>
 	);
 };
+
+const AdherenceHomeCard = ({ med, refreshMeds, medsThisDay }) => {
+	const [expanded, setExpanded] = useState(false);
+
+	const userToken = useSelector((state) => state.userToken.value);
+
+	const toggleExpanded = () => setExpanded(!expanded);
+
+	const medsCanTake = Math.min(Math.max(0, med.pillsTaken - med.pillsScheduled), medsThisDay);
+
+	const takeMed = async () => {
+		if (medsCanTake == 0)
+			return;
+
+		try {
+			await axios({
+				method: 'patch',
+				url: `${SERVER_URL}/patient/adherence/pills-taken`,
+				headers: {
+					Authorization: userToken,
+				},
+				data: {
+					patientMedicationId: med.patientMedicationId,
+					additionalPillsTaken: 1,
+				}
+			});
+
+			refreshMeds();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	return (
+		<Card color="#E1E1E8" depth={1} style={{ padding: 0 }}>
+
+			<Pressable onPress={toggleExpanded}>
+				<Card color="#D2D2DF" depth={1} style={{ padding: 0 }}>
+					<View style={{ flexDirection: "row" }}>
+						<Card depth={1} color={PRIMARY_COLOR} style={{ width: 96, height: 96, alignItems: 'center', justifyContent: 'center' }}>
+							<Image source={pill} style={{ width: 64, height: 64 }} />
+						</Card>
+						<View style={{ margin: 8, flex: 1 }}>
+							<View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+								<TextBody text="8:00 am" />
+								<Icon name={expanded ? "chevron-up" : "chevron-down"} size={25} />
+							</View>
+							<TextSubHeader2 text={med.medicationName} />
+							<TextNote text={`${medsCanTake} of ${medsThisDay} doses today`} />
+						</View>
+					</View>
+				</Card>
+			</Pressable>
+
+			{expanded &&
+				<View style={{ flexDirection: "row", margin: 4 }}>
+					<PrimaryButton label="Taken" onPress={takeMed} style={{ flex: 1, margin: 4 }} />
+					<PrimaryButton label="Skip" style={{ flex: 1, margin: 4 }} />
+					<PrimaryButton label="Reminder" style={{ flex: 1, margin: 4 }} />
+				</View>
+			}
+
+		</Card>
+	)
+}
