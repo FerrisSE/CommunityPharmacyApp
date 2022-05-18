20bf07e184de5c53f963940aafc2cac58fcbc1d1
Jacob Goeldel <goeldej@ferris.edu>
2022-05-18 09:51:44 -0400

    fixed dashboard bugs on android

--- screens/pharmacist/pharmacist-dashboard.js
+++ screens/pharmacist/pharmacist-dashboard.js
@@ -36,34 +36,32 @@ export const DashboardScreen = () => {
 	}, []);
 
 	return (
-		<View style={{ width: "100%", height: "100%", padding: 48 }}>
+		<View style={{ width: "100%", height: "100%", padding: 32 }}>
 			<TextHeader2 text="Dashboard" style={{ marginBottom: 8 }} />
 			<Card style={{ flex: 1, padding: 32, alignItems: "center" }} depth={0}>
-				<View style={{ width: "75%" }}>
-					<View style={{ width: "100%", flex: 1 }}>
-						<View style={{ width: "100%", alignItems: "center", justifyContent: "center", margin: 24 }}>
-							<TextHeader1 text="Community Pharmacy" style={{ color: PRIMARY_COLOR }} />
-							<View style={{ width: "75%", flexDirection: 'row', marginTop: 32, marginBottom: 32 }}>
-								<Input placeholder="Search App..." defaultText={searchText} setText={setSearchText} style={{ flex: 1, backgroundColor: WHITE }} />
-								<PrimaryButton label="Search" style={{ margin: 16, paddingLeft: 32, paddingRight: 32 }} />
-							</View>
+				<View style={{ width: "100%", flex: 1 }}>
+					<View style={{ width: "100%", alignItems: "center", justifyContent: "center", margin: 24 }}>
+						<TextHeader1 text="Community Pharmacy" style={{ color: PRIMARY_COLOR }} />
+						<View style={{ width: "75%", flexDirection: 'row', marginTop: 32, marginBottom: 32 }}>
+							<Input placeholder="Search App..." defaultText={searchText} setText={setSearchText} style={{ flex: 1, backgroundColor: WHITE }} />
+							<PrimaryButton label="Search" style={{ margin: 16, paddingLeft: 32, paddingRight: 32 }} />
 						</View>
 					</View>
-
-					<TextHeader3 text="Quick Links" />
-					<View style={{ width: "50%", flexDirection: 'row', margin: 12 }}>
-						<Card depth={2} style={{ margin: 12, flexDirection: "row", alignItems: "center", justifyContent: "center", padding: 32 }}>
-							<Icon name="format-list-bulleted" size={32} color={PRIMARY_COLOR} />
-							<TextSubHeader1 text="Schedule Appointments" style={{ color: PRIMARY_COLOR, marginLeft: 4 }} />
-						</Card>
-						<Card depth={2} style={{ margin: 12, flexDirection: "row", alignItems: "center", justifyContent: "center", padding: 32 }}>
-							<Icon name="account-multiple" size={32} color={PRIMARY_COLOR} />
-							<TextSubHeader1 text="Patient Search" style={{ color: PRIMARY_COLOR, marginLeft: 4 }} />
-						</Card>
-					</View>
-
-					<AppointmentsView appointments={appointments} loadAppointments={loadAppointments} viewedDay={moment()} viewLimit={3} />
 				</View>
+
+				<TextHeader3 text="Quick Links" />
+				<View style={{ width: "100%", flexDirection: 'row', margin: 12, alignItems: "flex-start" }}>
+					<Card depth={2} style={{ margin: 12, flexDirection: "row", alignItems: "center", justifyContent: "center", padding: 32 }}>
+						<Icon name="format-list-bulleted" size={32} color={PRIMARY_COLOR} />
+						<TextSubHeader1 text="Schedule Appointments" style={{ color: PRIMARY_COLOR, marginLeft: 4 }} />
+					</Card>
+					<Card depth={2} style={{ margin: 12, flexDirection: "row", alignItems: "center", justifyContent: "center", padding: 32 }}>
+						<Icon name="account-multiple" size={32} color={PRIMARY_COLOR} />
+						<TextSubHeader1 text="Patient Search" style={{ color: PRIMARY_COLOR, marginLeft: 4 }} />
+					</Card>
+				</View>
+
+				<AppointmentsView appointments={appointments} loadAppointments={loadAppointments} viewedDay={moment()} viewLimit={3} />
 			</Card>
 		</View>
 	)
