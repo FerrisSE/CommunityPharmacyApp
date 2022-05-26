f5a09a45b4407be374f565a68a4e9ae068648e1c
Jacob Goeldel <goeldej@ferris.edu>
2022-05-26 16:35:59 -0400

    better todo message on services

--- screens/pharmacist/services/pharmacist-services.js
+++ screens/pharmacist/services/pharmacist-services.js
@@ -1,10 +1,10 @@
 import { View } from "react-native"
-import { TextBody } from "../../../components/text"
+import { TextHeader2 } from "../../../components/text"
 
 export const PharmacistServicesScreen = () => {
 	return (
-		<View>
-			<TextBody text="main home page for services" />
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<TextHeader2 text="TODO: this screen is not done" />
 		</View>
 	)
 }
